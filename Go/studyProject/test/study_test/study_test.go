f6224983d008efaec2e837da5e22d5357b3322b7
ydchen <1362724852@qq.com>
2022-05-06 18:32:31 +0800

    add some content

--- Go/studyProject/test/study_test/study_test.go
+++ Go/studyProject/test/study_test/study_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
 	"testing"
 	"time"
 )
@@ -23,6 +26,7 @@ func TestName1(t *testing.T)  {
 	}
 }
 
+// channel 简单测试
 func TestName2(t *testing.T)  {
 	ch1 := make(chan int)
 	go func() {
@@ -40,6 +44,7 @@ func TestName2(t *testing.T)  {
 	}
 }
 
+// 类型声明
 func TestName3(t *testing.T) {
 	var a int
 	a = 0
@@ -57,6 +62,65 @@ func TestName3(t *testing.T) {
 	fmt.Println("size of array3: ", len(array3))
 }
 
+// 获取字典长度
 func TestName4(t *testing.T) {
+	ret := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+	fmt.Println(len(ret))
+}
+
+// sync.Map 测试
+func TestName5(t *testing.T)  {
+	var m sync.Map
+	// 1. 写入
+	m.Store("qcrao", 18)
+	m.Store("stefno", 20)
+
+	// 2. 读取
+	age, _ := m.Load("qcrao")
+	fmt.Println(age.(int))
+
+	// 3. 遍历
+	mapLen := 0
+	m.Range(func(key, value interface{}) bool {
+		name := key.(string)
+		age := value.(int)
+		fmt.Println(name, age)
+		mapLen += 1
+		fmt.Println(mapLen)
+		return true
+	})
+
+	// 4. 删除
+	m.Delete("qcrao")
+	age, ok := m.Load("qcrao")
+	fmt.Println(age, ok)
+
+	// 5. 读取或写入
+	m.LoadOrStore("stefno", 100)
+	age, _ = m.Load("stefno")
+	fmt.Println(age)
+}
+
+// 读取 json 文件
+func TestName6(t *testing.T) {
+	const dataFile = "files/snmp_mac_port.json"
+
+	// 打开json文件
+	jsonFile, err := os.Open(dataFile)
+
+	// 处理错误
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 关闭wenjian
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	fmt.Println(string(byteValue))
 
 }
